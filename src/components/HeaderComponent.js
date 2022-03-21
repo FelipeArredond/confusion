afc1313d75017b536a2562446498152ceee6ff10
FelipeArredond <pipechiqui77@gmail.com>
2022-03-21 16:46:19 -0500

    Correccion bug header

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -44,8 +44,8 @@ class Header extends Component{
                 <Navbar dark expand="md">
                     <div className="container">
                         <NavbarToggler onClick={this.toggleNav}/>
-                        <NavbarBrand className="mr-auto" href="/">
-                            <img src="assets/images/logo.png" height="30" width="41"
+                        <NavbarBrand className="mr-auto" href="/home">
+                            <img src="assets/MACOSX/images/._logo.png" height="30" width="41"
                                 alt="Ristorante Con Fusion" />
                         </NavbarBrand>
                         <Collapse isOpen={this.state.isNavOpen} navbar>
